51151ccc7345467aa5af8dc4fcf27df5dcc8038f
hesam <hesam.naderi.kotlin@gmail.com>
2024-07-10 11:43:02 +0330

    add:repository

---
+++ app/src/main/java/com/example/jetweatherforcast/repository/WeatherRepository.kt
@@ -0,0 +1,17 @@
+package com.example.jetweatherforcast.repository
+
+import com.example.jetweatherforcast.data.DataOrException
+import com.example.jetweatherforcast.model.Weather
+import com.example.jetweatherforcast.network.WeatherApi
+import javax.inject.Inject
+
+class WeatherRepository @Inject constructor(private val weatherApi: WeatherApi) {
+    suspend fun getWeather(cityQuery: String): DataOrException<Weather, Boolean, Exception> {
+        val response = try {
+            weatherApi.getWeather(cityQuery)
+        } catch (e: Exception) {
+            return DataOrException(e = e)
+        }
+        return DataOrException(data = response)
+    }
+}
